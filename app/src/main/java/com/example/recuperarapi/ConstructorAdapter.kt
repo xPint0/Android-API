c1aa3abbc6b943dd46e7beedb3ea9543e8142cd5
xPint0 <dpjretc@gmail.com>
2024-03-03 21:02:32 +0100

    Commit urls

--- app/src/main/java/com/example/recuperarapi/ConstructorAdapter.kt
+++ app/src/main/java/com/example/recuperarapi/ConstructorAdapter.kt
@@ -1,3 +1,5 @@
+import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -13,6 +15,23 @@ class ConstructorAdapter(private val constructors: List<Constructors>) :
         val nameTextView: TextView = itemView.findViewById(R.id.nameTextView)
         val nationalityTextView: TextView = itemView.findViewById(R.id.nationalityTextView)
         val urlTextView: TextView = itemView.findViewById(R.id.urlTextView)
+
+        init {
+            itemView.setOnClickListener {
+                val position = adapterPosition
+                if (position != RecyclerView.NO_POSITION) {
+                    val url = constructors[position].url
+                    openUrlInChrome(url)
+                }
+            }
+        }
+
+        private fun openUrlInChrome(url: String) {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.setClassName("com.android.chrome", "com.google.android.apps.chrome.Main")
+            intent.data = Uri.parse(url)
+            itemView.context.startActivity(intent)
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -25,7 +44,7 @@ class ConstructorAdapter(private val constructors: List<Constructors>) :
         val constructor = constructors[position]
         holder.nameTextView.text = constructor.name
         holder.nationalityTextView.text = "Nationality: " + constructor.nationality
-        holder.urlTextView.text = constructor.url
+        holder.urlTextView.text = "Biography: " + constructor.url
     }
 
     override fun getItemCount(): Int = constructors.size
--- app/src/main/java/com/example/recuperarapi/DriverAdapter.kt
+++ app/src/main/java/com/example/recuperarapi/DriverAdapter.kt
@@ -1,3 +1,5 @@
+import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,11 +13,28 @@ class DriverAdapter(private val drivers: List<Drivers>) :
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val givenNameTextView: TextView = itemView.findViewById(R.id.givenNameTextView)
-        //val familyNameTextView: TextView = itemView.findViewById(R.id.familyNameTextView)
         val nationalityTextView: TextView = itemView.findViewById(R.id.nationalityTextView)
         val dateofBirthTextView: TextView = itemView.findViewById(R.id.dateofBirthTextView)
         val urlTextView: TextView = itemView.findViewById(R.id.urlTextView)
+
+        init {
+            itemView.setOnClickListener {
+                val position = adapterPosition
+                if (position != RecyclerView.NO_POSITION) {
+                    val url = drivers[position].url
+                    openUrlInChrome(url)
+                }
+            }
+        }
+
+        private fun openUrlInChrome(url: String) {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.setClassName("com.android.chrome", "com.google.android.apps.chrome.Main")
+            intent.data = Uri.parse(url)
+            itemView.context.startActivity(intent)
+        }
     }
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context)
